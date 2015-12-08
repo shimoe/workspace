f485b60e38b6a19bebaa76b275cb7ab407d3b577
shimomatsubae kouta <o104064k@tb019.p.isc.kyutech.ac.jp>
2015-12-08 12:05:23 +0900

    add project

---
+++ test/Debug/src/gpio.d
@@ -0,0 +1,13 @@
+src/gpio.o src/gpio.d: ../src/gpio.c \
+ /home/t/o104064k/robocar/workspace/test/lib/LPC13xx.h \
+ /home/t/o104064k/robocar/workspace/test/lib/core_cm3.h \
+ /home/t/o104064k/robocar/workspace/test/lib/system_LPC13xx.h \
+ /home/t/o104064k/robocar/workspace/test/inc/gpio.h
+
+/home/t/o104064k/robocar/workspace/test/lib/LPC13xx.h:
+
+/home/t/o104064k/robocar/workspace/test/lib/core_cm3.h:
+
+/home/t/o104064k/robocar/workspace/test/lib/system_LPC13xx.h:
+
+/home/t/o104064k/robocar/workspace/test/inc/gpio.h:
---
+++ test/Debug/src/ixbus.d
@@ -0,0 +1,16 @@
+src/ixbus.o src/ixbus.d: ../src/ixbus.c \
+ /home/t/o104064k/robocar/workspace/test/lib/LPC13xx.h \
+ /home/t/o104064k/robocar/workspace/test/lib/core_cm3.h \
+ /home/t/o104064k/robocar/workspace/test/lib/system_LPC13xx.h \
+ /home/t/o104064k/robocar/workspace/test/inc/type.h \
+ /home/t/o104064k/robocar/workspace/test/inc/ixbus.h
+
+/home/t/o104064k/robocar/workspace/test/lib/LPC13xx.h:
+
+/home/t/o104064k/robocar/workspace/test/lib/core_cm3.h:
+
+/home/t/o104064k/robocar/workspace/test/lib/system_LPC13xx.h:
+
+/home/t/o104064k/robocar/workspace/test/inc/type.h:
+
+/home/t/o104064k/robocar/workspace/test/inc/ixbus.h:
